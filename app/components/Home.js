c1067d0ff3d96ca5008a1055777fa37b3fb0d154
Berryhill <matthewberryhill@gmail.com>
2016-08-26 16:22:04 +0700

    fixed the main wrapper

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,17 +1,17 @@
 var React = require('react');
 var ReactRouter = require('react-router');
-var MainContainer = require('./MainWrapper');
+var MainWrapper = require('./MainWrapper');
 
 var Home = React.createClass({
   render: function () {
     return (
-      <MainContainer>
+      <MainWrapper>
         <h1>Github Battle</h1>
         <p className='lead'>May the Battle of the Hubs COMMENCE!</p>
         <Link to='/playerOne'>
           <button type='button' className='btn btn-lg btn-success'>Get Started</button>
         </Link>
-      </MainContainer>
+      </MainWrapper>
     )
   }
 });
