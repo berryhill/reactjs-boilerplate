38e4480c285b96f30cf55f1bb4edd61f133a63a0
Berryhill <matthewberryhill@gmail.com>
2016-08-26 16:23:42 +0700

    fixing main wrapper again

--- app/components/Home.js
+++ app/components/Home.js
@@ -6,11 +6,8 @@ var Home = React.createClass({
   render: function () {
     return (
       <MainWrapper>
-        <h1>Github Battle</h1>
-        <p className='lead'>May the Battle of the Hubs COMMENCE!</p>
-        <Link to='/playerOne'>
-          <button type='button' className='btn btn-lg btn-success'>Get Started</button>
-        </Link>
+        <h1>Home</h1>
+        <p className='lead'>This is the beginning of your application!</p>
       </MainWrapper>
     )
   }
